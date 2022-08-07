93c7f4afab6dc5315f570a9f482a36d7f4e753b7
PrakashSowdha <prakashsrk@gmail.com>
2022-08-07 18:47:52 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,47 +11,14 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
-  fetch('http://localhost:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
-
-//
-// Goal: Render content to paragraphs
-//
-// 1. Select the second message p from JavaScript
-// 2. Just before fetch, render loading message and empty p
-// 3. If error, render error
-// 4. If no error, render location and forecast
-// 5. Test your work! Search for errors and for valid locations
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
-//
-// Goal: Fetch weather!
-//
-// 1. Setup a call to fetch to fetch weather for Boston
-// 2. Get the parse JSON response
-// -- If error property, print error
-// -- If no error property, print location and forecast
-// 3. Refresh the browser and test your work.
-
-//
-// Goal: Use input value to get weather
-//
-// 1. Migrate fetch call into the submit callback
-// 2. Use the search text as the address query string value
-// 3. Submit the form with a valid and invalid value to test
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -40,59 +41,6 @@ app.get('/help', (req, res) => {
     name: 'Andrew Mead',
   });
 });
-
-//
-// Goal-1: Setup two new routes
-//
-// 1. Setup an about route and render a page title
-// 2. Setup a weather route and render a page title
-// 3. Test your work by visiting both in the browser
-
-//
-// Goal-2: Update routes
-//
-// 1. Setup about route to render a title with HTML
-// 2. Setup a weather route to send back JSON
-// -- Object with forecast and location strings
-// 3. Test your work by visiting both in the browser
-
-//
-// Goal-3: Create two more HTML files
-//
-// 1. Create a html page for about with "About" title
-// 2. Create a html page for about with "Help" title
-// 3. Remove the old route handlers for both
-// 4. Visit both in the browser to test your work.
-
-//
-// Goal-4: Create a template for help page
-//
-// 1. Setup a help template to render a help message to the screen
-// 2. Setup the help route and render the template with an example message
-// 3. Visit the route in the browser and see your help message print.
-
-//
-// Goal-5: Create a partial for the footer
-//
-// 1. Setup the template for the footer partial "Created by Some Name"
-// 2. Render the partial at the bottom of all three pages
-// 3. Test your work by visiting all three pages
-
-//
-// Goal-7: Update weather endpoint to accept address
-//
-// 1. No address? Send back an error message
-// 2. Address? Send back the static JSON
-// -- Add address properly onto JSON which returns the provided address
-// 3. Test /weather and /weather?address=philadelphia
-
-//
-// Goal-8: Wire up / weather
-//
-// 1. Require geocode/forecast into app.js
-// 2. Use the address to geocode.
-// 3. Use the coordinates to get forecast.
-// 4. Send back the real forecast and location.
 
 app.get('/weather', (req, res) => {
   if (!req.query.address) {
@@ -136,10 +84,6 @@ app.get('/products', (req, res) => {
   });
 });
 
-// app.com
-// app.com/help
-// app.com/about
-
 app.get('/help/*', (req, res) => {
   res.render('404', {
     title: '404',
@@ -156,16 +100,6 @@ app.get('*', (req, res) => {
   });
 });
 
-//
-// Goal-6: Create and render a 404 page with handlebars
-//
-// 1. Setup the template to render the header and footer
-// 2. Setup the template to render an error message in a paragraph
-// 3. Render the template for both 404 routes
-// -- Page not found.
-// -- Help article not found.
-// 4. Test your work. Visit /What and /help/units
-
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
 });
